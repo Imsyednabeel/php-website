2004896e59f8c1521eaf95cdb3fae91936235e95
Imsyednabeel <nabeelsyed770@gmail.com>
2025-06-27 12:15:28 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Simple PHP Website</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Website!</h1>
+    <p>This page is deployed using AWS CodePipeline and Auto Scaling 🚀</p>
+</body>
+</html>
