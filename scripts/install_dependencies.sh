853fef0a66e3b87355a848df69f8bbb9006156ce
Imsyednabeel <nabeelsyed770@gmail.com>
2025-06-30 18:17:54 +0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# You can install Composer dependencies or other tools here
-# For example:
-# cd /var/www/html/
-# curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-# composer install --no-dev --no-interaction --prefer-dist
+# Install Apache HTTP Server
+sudo dnf install -y httpd
